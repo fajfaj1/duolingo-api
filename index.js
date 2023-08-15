f3d4e87ee1467701b82054a2aaf57a33ff7ea483
fajfaj1 <97434565+fajfaj1@users.noreply.github.com>
2023-08-15 18:40:59 +0200

    use letsencrypt certificates

--- index.js
+++ index.js
@@ -89,8 +89,8 @@ https.createServer(
 		// Provide the private and public key to the server by reading each
 		// file's content with the readFileSync() method.
     {
-      key: fs.readFileSync("./ssl/key.pem"),
-      cert: fs.readFileSync("./ssl/cert.pem"),
+      key: fs.readFileSync("./ssl/privkey.pem"),
+      cert: fs.readFileSync("./ssl/fullchain.pem"),
     },
     app
   )
