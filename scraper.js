11eb8fc4df515fc5794ec6261de5979925cea5ed
fajfaj1 <97434565+fajfaj1@users.noreply.github.com>
2023-08-22 18:36:59 +0200

    modernization

--- scraper.js
+++ scraper.js
@@ -2,6 +2,7 @@ import puppeteer from 'puppeteer';
 import log from './modules/logs.js';
 import { getLanguageFlag } from './modules/flagger.js';
 import { download, generate } from './modules/avatars.js';
+import chalk from 'chalk';
 
 const imgSize = 'xlarge'
 // Scraper() returns fetchProfile()
@@ -74,64 +75,175 @@ export default async function scraper() {
             })
 
             function responseToData(profile) {
-                console.log(profile)
-
-                const props = ['name', 'totalXp', 'username', 'courses', 'streak', 'currentCourseId', 'createionDate', 'streakData'
-                    , 'hasPlus', 'picture', 'id', 'tier', 'streak_in_tier', 'top_three_finishes']
-                Object.keys(profile).forEach((key, index) => {
-                    if (!props.includes(key)) {
-                        delete profile[key]
-                    }
-                })
-
-                const courseProps = ['title', 'learningLanguage', 'xp', 'healthEnabled', 'fromLanguage', 'crowns', 'id']
-                const courses = profile.courses
-                courses.forEach((course, index) => {
-                    Object.keys(course).forEach((key, index) => {
-                        if (!courseProps.includes(key)) {
-                            delete course[key]
+                // Objective: Parse the response to a clean object (return it)
+                const template = {
+                    name: "name",
+                    user: {
+                        username: "username",
+                        displayname: "name",
+                        totalXp: "totalXp",
+                        streak: "streak",
+                        currentCourseId: "currentCourseId",
+                        id: "id",
+                        hasPlus: "hasPlus",
+                        picture: "picture",
+                    },
+                    leaderboard: {
+                        league: "tier",
+                        wins: {
+                            global: "num_wins",
+                            1: "stats['number_one_finishes']",
+                            2: "stats['number_two_finishes']",
+                            3: "stats['top_three_finishes']",
+                        },
+                        streakInLeague: "streak_in_tier",
+                    },
+                    days: [
+                        'summaries',
+                        {
+                            gainedXp: "summaries[i].gainedXp",
+                            date: "summaries[i].date",
+                            frozen: "summaries[i].frozen",
+                            repaired: "summaries[i].repaired",
+                            numSessions: "summaries[i].numSessions",
+                            totalSessionTime: "summaries[i].totalSessionTime"
                         }
-                    })
-                })
-                profile.courses = courses
-
-                profile.coursesCount = courses.length
-
-                const profileCourses = profile.courses
-                profileCourses.forEach(course => {
-
-                    const flag = getLanguageFlag(course.title)
-                    course.flag = flag
-
-                })
-                profile.courses = profileCourses
-
-                profile.status = 'success'
-
-                const avatarUrl = 'https:' + profile.picture + `/${imgSize}`
-
-                let filePath = 'null'
-
-                if (avatarUrl != `https://simg-ssl.duolingo.com/avatar/default_2/${imgSize}`) {
-                    const fileName = username + '.png'
-                    filePath = `/public/avatars/${fileName}`
-                    download(avatarUrl, fileName)
-                } else {
-                    let firstLetter = 'null'
-                    if (profile.name != null) {
-                        firstLetter = profile.name.charAt(0)
-                    } else {
-                        firstLetter = profile.username.charAt(0)
-                    }
-                    const fileName = firstLetter + '.png'
-                    filePath = `/public/avatars/default/${fileName}`
-
-                    log('First letter', `First letter is ${firstLetter}`, 'info')
-                    generate(firstLetter, browser)
+                    ],
+                    courses: [
+                        'courses',
+                        {
+                            title: "courses[i].title",
+                            learningLanguage: "courses[i].learningLanguage",
+                            courseXp: "courses[i].xp",
+                            healthEnabled: "courses[i].healthEnabled",
+                            fromLanguage: "courses[i].fromLanguage",
+                            crowns: "courses[i].crowns",
+                            id: "courses[i].id"
+                        }
+                    ]
                 }
 
+                /* The plan:
+                    1. Loop over a layer
+                    2. Work on it
+                    > 2.1. If it's a string, just add it to the response
+                    > 2.2. If it's an object, loop over it (with the same function)
+                    > 2.3. If it's an array, loop over it (with the same function)
 
-                profile.picture = `https://${hostname}${filePath}`
+                */
+                let formattedProfile = {}
+                formattedProfile = layer(template)
+
+                function layer(obj, index) {
+                    const thisLayer = {}
+                    const workOn = {
+                        string: (key, string) => {
+                            // Get the value and attach to the layer
+                            let valuePath = `profile.${string}`
+
+                            let val = 'null'
+                            try {
+                                val = eval(valuePath)
+                            } catch (e) {
+                                log('Error', `Error while evaluating ${string}`, 'error')
+                            }
+
+                            thisLayer[key] = val
+                        },
+                        object: (key, object) => {
+                            // Go deeper
+                            thisLayer[key] = layer(object)
+                        },
+                        array: (key, arrayTemplate) => {
+                            // Get the keyname from the template and use it to get the array from the profile
+                            const keyInProfile = arrayTemplate[0]
+                            const arrayFromProfile = profile[keyInProfile] || []
+
+                            const arr = []
+
+                            arrayFromProfile.forEach((e, index) => {
+                                // element is 
+                                let object = arrayTemplate[1]
+                                object = JSON.stringify(object)
+                                object = object.replace(/\[i\]/g, `[${JSON.stringify(index)}]`)
+                                object = JSON.parse(object)
+
+                                arr.push(layer(object, index))
+                            });
+                            thisLayer[key] = arr
+                        }
+                    }
+                    // Keys like user, leaderboard, days, courses
+                    const keys = Object.keys(obj)
+                    keys.forEach(key => {
+                        const value = obj[key]
+
+                        const isString = typeof value === 'string'
+                        const isArray = value instanceof Array
+                        const isObject = typeof value === 'object' && !isArray
+
+                        if (isString) {
+                            workOn.string(key, value)
+                        } else if (isArray) {
+                            workOn.array(key, value)
+                        } else if (isObject) {
+                            workOn.object(key, value)
+                        }
+                    })
+                    return thisLayer
+                }
+                formattedProfile = applyProfileExceptions(formattedProfile)
+
+                return formattedProfile
+            }
+
+            function applyProfileExceptions(profile) {
+                // Courses count
+                const courses = profile.courses || []
+                profile.coursesCount = courses.length
+
+                // Course flags
+                profile.courses = courses.map(course => {
+                    const flag = getLanguageFlag(course.title)
+                    course.flag = flag
+                    return course
+                })
+
+                function sortCourses(courses) {
+                    const sortedCourses = courses.sort((a, b) => {
+                        return b.courseXp - a.courseXp
+                    })
+                    return sortedCourses
+                }
+                profile.courses = sortCourses(profile.courses)
+
+                function getAvatarUrl(profile) {
+                    const avatarUrl = 'https:' + profile.user.picture + `/${imgSize}`
+
+                    let filePath = 'null'
+
+                    if (avatarUrl != `https://simg-ssl.duolingo.com/avatar/default_2/${imgSize}`) {
+                        const fileName = username + '.png'
+                        filePath = `/public/avatars/${fileName}`
+                        download(avatarUrl, fileName)
+                    } else {
+                        let firstLetter = 'null'
+                        if (profile.name != null) {
+                            firstLetter = profile.user.displayname.charAt(0)
+                        } else {
+                            firstLetter = profile.user.username.charAt(0)
+                        }
+                        const fileName = firstLetter + '.png'
+                        filePath = `/public/avatars/default/${fileName}`
+
+                        generate(firstLetter, browser)
+                    }
+
+
+                    return `https://${hostname}${filePath}`
+                }
+
+                profile.user.picture = getAvatarUrl(profile)
 
                 return profile
             }
